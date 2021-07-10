b783f1f71ae38b4182ef8ef74cf8dd9217c8de82
Mustafa Özkarabulut <81626732+MustafaOzkarabulut@users.noreply.github.com>
2021-07-10 21:14:38 +0300

    SignedIn SignedOut eklendi

---
+++ src/layouts/SignedIn.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Dropdown, Image, Menu } from 'semantic-ui-react'
+
+export default function SignedIn() {
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src="https://vz.cnwimg.com/wp-content/uploads/2012/05/mark.jpg?x13500"></Image>
+                <Dropdown pointing="top left" text="Mustafa">
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item text="Çıkış Yap" icon="sign-out"/>    
+                        
+                    </Dropdown.Menu>
+                </Dropdown>
+           </Menu.Item>
+        </div>
+    )
+}
