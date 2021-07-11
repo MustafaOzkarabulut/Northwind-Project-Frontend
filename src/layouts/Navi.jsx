2decd0048b3ad8be01085ee4aac025ea20972110
Mustafa Özkarabulut <81626732+MustafaOzkarabulut@users.noreply.github.com>
2021-07-11 13:22:12 +0300

    React Rooter eklendi

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import CartSummary from "./CartSummary";
 import { Button,Container, Menu} from 'semantic-ui-react'
 import SignedOut from "./SignedOut";
 import SignedIn from "./SignedIn";
+import { useHistory } from "react-router-dom";
 
 export default function Navi() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const history = useHistory()
+  function handleSignOut(params) {
+    setIsAuthenticated(false)
+    history.push("/")
+  }
+
+  function handleSignIn(params) {
+    setIsAuthenticated(true)
+  }
+
   return (
     <div>
       <Menu inverted fixed="top">
@@ -20,8 +32,7 @@ export default function Navi() {
 
         <Menu.Menu position="right">
           <CartSummary/>
-            <SignedOut/>
-            <SignedIn/>
+          {isAuthenticated?<SignedIn signOut={handleSignOut} bisey="1"/>:<SignedOut signIn={handleSignIn} bisey="2"/>}      
         </Menu.Menu>
         </Container>
       </Menu>
